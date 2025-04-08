d72efaad30d5720718bb9e79adc64163f0b177b1
Aindrila0-03 <aindrilatalukder@gmail.com>
2025-04-09 02:49:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def index():
     return render_template('index.html', prediction=prediction, img_path=img_path, is_correct=is_correct)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
